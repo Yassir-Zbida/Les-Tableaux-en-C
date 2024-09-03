bc649a38bcc129eb9ec8157e2e8937dc52a25e4e
Yassir <71879806+zruzeeyty@users.noreply.github.com>
2024-09-03 21:34:37 +0100

    Create Ex04.c
    Ex 04 Solution

---
+++ Ex04.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+
+int Tab[100] ;
+int i , Taille , Min ;
+printf("Veuillez saisir La taille de tableau :");
+scanf("%d", &Taille);
+for (i=0 ; i<Taille ; i++){
+    printf("Tab[%d] = ",i+1);
+    scanf("%d",&Tab[i]);
+}
+Min = Tab [0];
+for (i=1 ; i<Taille ; i++){
+    if(Min>Tab[i]){
+        Min = Tab [i];
+    }
+}
+printf("Le Minimum De Votre Tableau est : %d\n",Min);
+
+return 0 ;
+}
